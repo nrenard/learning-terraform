bbaea9455138da400f737430a49003b4284a40d2
Nicolas Renard <nicolasrenard1999@gmail.com>
2024-09-25 11:20:14 -0300

    Trying ALB

--- main.tf
+++ main.tf
@@ -38,7 +38,8 @@ resource "aws_instance" "blog" {
   subnet_id =  module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -50,53 +51,11 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  # Security Group
-  # security_group_ingress_rules = {
-  #   all_http = {
-  #     from_port   = 80
-  #     to_port     = 80
-  #     ip_protocol = "tcp"
-  #     description = "HTTP web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  #   all_https = {
-  #     from_port   = 443
-  #     to_port     = 443
-  #     ip_protocol = "tcp"
-  #     description = "HTTPS web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  # }
-  # security_group_egress_rules = {
-  #   all = {
-  #     ip_protocol = "-1"
-  #     cidr_ipv4   = "10.0.0.0/16"
-  #   }
-  # }
-
   listeners = {
-    # ex-http-https-redirect = {
-    #   port     = 80
-    #   protocol = "HTTP"
-    #   redirect = {
-    #     port        = "443"
-    #     protocol    = "HTTPS"
-    #     status_code = "HTTP_301"
-    #   }
-    # }
-    # ex-http-https-redirect = {
-    #   port     = 80
-    #   protocol = "HTTP"
-    #   redirect = {
-    #     port        = "443"
-    #     protocol    = "HTTPS"
-    #     status_code = "HTTP_301"
-    #   }
-    # }
     http = {
       port     = 80
       protocol = "HTTP"
-      
+
       forward = {
         target_group_key = "instance"
       }
@@ -114,8 +73,8 @@ module "alb" {
   }
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -131,4 +90,9 @@ module "blog_sg" {
 
   egress_rules        = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
