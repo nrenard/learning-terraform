d07e3459d99a3cb1ea5c7661ae82b316afbf3762
Nicolas Renard <nicolasrenard1999@gmail.com>
2024-09-25 14:57:59 -0300

    Trying Autoscalling

--- main.tf
+++ main.tf
@@ -31,15 +31,15 @@ module "blog_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
+  version = "6.5.2"
   
   name     = "blog"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_groups_arns
   security_groups     = [module.blog_sg.security_group_id]
-  # target_groups_arns = module.blog_alb.target_groups_arns
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -77,6 +77,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      # target_id        = module.autoscaling.autoscaling_group_id
     }
   }
 
