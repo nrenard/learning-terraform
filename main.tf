a53eb149cbc1e8021dae6dafa4456a1eb77894e1
Nicolas Renard <nicolasrenard1999@gmail.com>
2024-09-25 14:49:21 -0300

    Trying Autoscalling

--- main.tf
+++ main.tf
@@ -77,7 +77,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      # target_id        = module.autoscaling.autoscaling_group_id
     }
   }
 
