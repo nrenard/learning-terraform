2301832b020bfaea56d989ce6603283fef0b8f2e
Nicolas Renard <nicolasrenard1999@gmail.com>
2024-09-25 10:59:18 -0300

    Trying ALB

--- main.tf
+++ main.tf
@@ -14,11 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
